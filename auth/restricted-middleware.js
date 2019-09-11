64e44884420fdb367b43cb1e11907847dbae22ac
Tyler Turnipseed <tyler@techturnip.us>
2019-09-11 19:10:14 -0400

    created endpoint to get users only if the users share a dept with the user that makes the request

--- auth/restricted-middleware.js
+++ auth/restricted-middleware.js
@@ -6,20 +6,28 @@ const jwt = require('jsonwebtoken')
 // EXPORT =========================================|
 // ================================================|
 module.exports = (req, res, next) => {
-  const token = req.headers.authorization
+  let token
 
-  if (token) {
-    jwt.verify(token, process.env.JWT_SECRET, (err, decodedToken) => {
-      if (err) {
-        console.log('failed verify', err)
-        res.status(401).json({
-          message: 'Not verified'
-        })
-      } else {
-        // token is valid
-        req.decodedToken = decodedToken
-        next()
-      }
+  if (req.headers.authorization) {
+    token = req.headers.authorization
+
+    if (token) {
+      jwt.verify(token, process.env.JWT_SECRET, (err, decodedToken) => {
+        if (err) {
+          console.log('failed verify', err)
+          res.status(401).json({
+            message: 'Verification failed'
+          })
+        } else {
+          // token is valid
+          req.decodedToken = decodedToken
+          next()
+        }
+      })
+    }
+  } else {
+    res.status(401).json({
+      message: 'Please log in and try again'
     })
   }
 }
--- users/users-router.js
+++ users/users-router.js
@@ -12,7 +12,19 @@ const restricted = require('../auth/restricted-middleware.js')
 // DEFINE ENDPOINTS ===============================|
 // ================================================|
 router.get('/', restricted, (req, res) => {
-  const { sub, department } = req.decodedToken
+  const { department } = req.decodedToken
+
+  Users.findBy({ department })
+    .then(users => {
+      // remove password before sending data back
+      // to the client
+      users.forEach(user => {
+        delete user.password
+      })
+
+      res.json(users)
+    })
+    .catch(err => res.send(err))
 })
 // ------------------------------------------------|
 // EXPORT =========================================|
